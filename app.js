053e169605468b163e15c7ae260429847c3b0733
JuzoSp <speyerhasina@gmail.com>
2024-03-28 13:58:17 +0300

    commit mety

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
+const session = require('express-session');
 
 const app = express();
 const port = 8888;
+
+// Configuration de la session (express session)
+app.use(session({
+    secret: 'asezqsrs%&AB254yyy',
+    resave: false,
+    saveUninitialized: true
+}));
 
 const db = mysql.createConnection({
     host: '127.0.0.1',
@@ -15,10 +23,22 @@ const db = mysql.createConnection({
 // Connect to MySQL
 db.connect((err) => {
     if (err) {
+        console.log('Error while connecting...', err);
         throw err;
     }
     console.log('Connected to database');
 });
+
+
+//express static pour servir les fichier statics
+app.use(express.static('/', {
+    setHeaders: (res, path, stat) => {
+        if (path.endsWith('.css')) {
+            res.setHeader('Content-Type', 'text/css');
+        }
+    }
+}));
+
 
 // middleware CORS pour permettre les requêtes cross-origin
 app.use(cors());
@@ -36,6 +56,9 @@ app.post('/submit', (req, res) => {
     const { name, email, num, message } = req.body;
     const formData = { name, email, num, message };
 
+    // Stockage des données dans la session (session express)
+    req.session.formData = formData;
+
     // Insertion des données dans la base de données
     db.query('INSERT INTO contacts SET ?', formData, (err, result) => {
         if (err) {
@@ -44,6 +67,12 @@ app.post('/submit', (req, res) => {
             res.sendFile(__dirname + '/success.html');
         }
     });
+});
+
+// Route pour afficher les données du formulaire sur une autre page
+app.get('/display', (req, res) => {
+    const formData = req.session.formData;
+    res.sendFile(__dirname + '/display.html');
 });
 
 app.listen(port, () => {
